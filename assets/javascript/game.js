eabfae8c7ab13a0e57a1778d256240b9be631a1d
Blake Robert <Blaker.blaker4@gmail.com>
2018-07-21 15:39:20 -0400

    added animations to battle

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ var game = {
         { name: 'Dark Jedi', image: 'assets/images/Dark-Jedi.png', HP: '120', BaseAP: '25', CurrentAP: '25', CAP: '25' },
         { name: 'Darth Malak', image: 'assets/images/darth-malak.jpg', HP: '140', BaseAP: '15', CurrentAP: '15', CAP: '30' },
         { name: 'Darth Revan', image: 'assets/images/Darth-Revan.jpg', HP: '160', BaseAP: '10', CurrentAP: '10', CAP: '35' },
+        // { name: 'Darth Robert', image: 'assets/images/Darth-Bane.jpg', HP: '1800', BaseAP: '5', CurrentAP: '5', CAP: '40' },
     ],
 
 
@@ -38,8 +39,13 @@ var game = {
 
             var HPDiv = document.createElement("div");
             targetDiv2.appendChild(HPDiv);
-            HPDiv.innerHTML = " HP:" + $('#' + i).attr("data-HP") + "  AP:" + $('#' + i).attr("data-CurrentAP") + "  CAP:" + $('#' + i).attr("data-CAP");
+            HPDiv.innerHTML = "Health:" + $('#' + i).attr("data-HP") +"<br>" + "Attack Power:" + $('#' + i).attr("data-CurrentAP")+ "<br>" + "Counter-Attack:" + $('#' + i).attr("data-CAP");
             HPDiv.setAttribute('class', 'charStats');
+
+            var overlayDiv = document.createElement("div");
+            targetDiv2.appendChild(overlayDiv);
+            overlayDiv.setAttribute('class', 'overlay');
+            // overlayDiv. display:none");
 
 
 
@@ -78,7 +84,7 @@ $("#charSelarea").on("click", ".charavatar", function () {
         .appendTo("#selectedCharacter");
     var newHP2 = $('.attacker').attr('data-HP');
     var newAP = $('.attacker').attr('data-CurrentAP');
-    $('.attacker .charStats').text('HP:' + newHP2 + ' Attack Power:' + newAP);
+    $('.attacker .charStats').html('Health:' + newHP2 + "<br>" + 'Attack Power:' + newAP);
 
 
     isCharSelected = true;
@@ -87,7 +93,7 @@ $("#charSelarea").on("click", ".charavatar", function () {
         var newHP = $('#' + i).attr('data-HP');
         var newCAP = $('#' + i).attr('data-CAP');
         var targetDiv = $('#' + i + ' .charStats');
-        targetDiv.text(' HP:' + newHP + ' Counter Attack:' + newCAP)
+        targetDiv.html('Health:' + newHP + "<br>" + 'Counter Attack:' + newCAP)
 
     }
 
@@ -95,6 +101,15 @@ $("#charSelarea").on("click", ".charavatar", function () {
     $('.charavatar').removeClass('charavatar')
         .addClass('enemy');
 });
+
+function removeClass(){
+    $('.defender .overlay').removeClass('damageOverlay');
+    $('.attacker .overlay').removeClass('damageOverlay');
+}
+function emptyDiv(){
+    $('#defenderTile').empty();
+    
+}
 $("#enemies").on("click", ".enemy", function () {
     if (activeDefender === false) {
         $(this)
@@ -109,6 +124,8 @@ $("#enemies").on("click", ".enemy", function () {
 $('#attack-btn').on('click', function () {
     if ($('.attacker').attr('data-HP') > 1) {
         if (activeDefender === true) {
+            $('.defender .overlay').addClass('damageOverlay');
+            setTimeout(removeClass, 1001);
             $('#atkinfo').empty();
             $('#catkinfo').empty();
             $('.defender').attr('data-HP', function (i, origValue) {
@@ -116,15 +133,18 @@ $('#attack-btn').on('click', function () {
                 var newHP2 = $('.attacker').attr('data-HP');
                 var newCAP = $('.defender').attr('data-CAP');
                 var newAP = $('.attacker').attr('data-CurrentAP');
-                $('.defender .charStats').text(' HP:' + newHP + ' Counter Attack:' + newCAP);
-                $('.attacker .charStats').text('HP:' + newHP2 + ' Attack Power:' + newAP);
+                $('.defender .charStats').html('Health:' + newHP +"<br>" + 'Counter Attack:' + newCAP);
+                $('.attacker .charStats').html('Health:' + newHP2 +"<br>" + 'Attack Power:' + newAP);               
+                
                 return newHP;
             })
             if ($('.defender').attr('data-HP') > 1) {
                 $('.attacker').attr('data-HP', function (i, origValue) {
+                    $('.attacker .overlay').addClass('damageOverlay');
+                        setTimeout(removeClass, 1001);
                     var newHP = origValue - $('.defender').attr('data-cap');
                     var newAP = $('.attacker').attr('data-CurrentAP');
-                    $('.attacker .charStats').text('HP:' + newHP + ' Attack Power:' + newAP);
+                    $('.attacker .charStats').html('Health:' + newHP +"<br>" + 'Attack Power:' + newAP);
                     return newHP;
                 })
             }
@@ -135,7 +155,7 @@ $('#attack-btn').on('click', function () {
             $('.attacker').attr('data-CurrentAP', function (i, origValue) {
                 var newAP = +origValue + +$('.attacker').attr('data-BaseAP');
                 var newHP = $('.attacker').attr('data-HP');
-                $('.attacker .charStats').text('HP:' + newHP + ' Attack Power:' + newAP);
+                $('.attacker .charStats').html('Health:' + newHP +"<br>" + 'Attack Power:' + newAP);
                 return newAP;
             })
         }
@@ -144,7 +164,8 @@ $('#attack-btn').on('click', function () {
             $('#atkinfo').empty();
             $('#catkinfo').empty();
             $('#atkinfo').append('You have defeated ' + $('.defender').attr('data-name') + ', you can choose to fight another enemy.');
-            $('#defenderTile').empty();
+            $('.defender').addClass('loser');
+            setTimeout(emptyDiv, 900);
             activeDefender = false;
             enemiesDefeated++;
         }
@@ -154,10 +175,11 @@ $('#attack-btn').on('click', function () {
             $('#atkinfo').append('You have been defeated by ' + $('.defender').attr('data-name') + ' ...GAME OVER!!!');
             $('#catkinfo').append("<button onClick='game.reset()'>Restart Game</button>");
         }
-        if (enemiesDefeated > 2) {
+        if (enemiesDefeated === game.character.length - 1) {
             $('#atkinfo').empty();
             $('#catkinfo').empty();
-            $('#atkinfo').append('You WON!!!!! GAME OVER!!!');
+            $('.attacker').addClass('winner');
+            $('#atkinfo').append('You WON!!!!!');
             $('#catkinfo').append("<button onClick='game.reset()'>Restart Game</button>");
         }
     }
